337274cb844f0cae6f73a30d00396b7d3961117f
Matias Tripode <matiastripode@gmail.com>
2020-04-29 18:26:45 -0400

    Refactoring: grouped asserts by rules cases

--- src/test/java/FizzbuzzTest.java
+++ src/test/java/FizzbuzzTest.java
@@ -7,45 +7,39 @@ import org.junit.Assert;
 * For numbers which are multiples of both three and five print “FizzBuzz “.
 * */
 public class FizzbuzzTest {
+
+
     @Test
-    public void test_fizzbuzz_of_zero_should_return_zero() {
+    public void test_fizzbuzz_of_non_divisible_by_3_nor_by_5_should_return_number() {
         Assert.assertEquals("0", fizzbuzz(0));
-    }
-
-    @Test
-    public void test_fizzbuzz_of_one_should_return_one() {
         Assert.assertEquals("1", fizzbuzz(1));
-    }
-
-    @Test
-    public void test_fizzbuzz_of_two_should_return_two() {
         Assert.assertEquals("2", fizzbuzz(2));
-    }
-
-    @Test
-    public void test_fizzbuzz_of_three_should_return_fizz() {
-        Assert.assertEquals("fizz", fizzbuzz(3));
-    }
-
-    @Test
-    public void test_fizzbuzz_of_four_should_return_four() {
         Assert.assertEquals("4", fizzbuzz(4));
     }
 
     @Test
-    public void test_fizzbuzz_of_five_should_return_buzz() {
-        Assert.assertEquals("buzz", fizzbuzz(5));
-    }
-
-    @Test
-    public void test_fizzbuzz_of_six_should_return_fizz() {
+    public void test_fizzbuzz_of_divisible_by_5_should_return_fizz() {
+        Assert.assertEquals("fizz", fizzbuzz(3));
         Assert.assertEquals("fizz", fizzbuzz(6));
+        Assert.assertEquals("fizz", fizzbuzz(9));
     }
 
     @Test
-    public void test_fizzbuzz_of_fifthen_should_return_fizzbuzz() {
-        Assert.assertEquals("fizzbuzz", fizzbuzz(15));
+    public void test_fizzbuzz_of_divisible_by_5_should_return_buzz() {
+        Assert.assertEquals("buzz", fizzbuzz(5));
+        Assert.assertEquals("buzz", fizzbuzz(10));
+        Assert.assertEquals("buzz", fizzbuzz(20));
+        Assert.assertEquals("buzz", fizzbuzz(35));
     }
+
+
+    @Test
+    public void test_fizzbuzz_of_divisible_by_3_and_5_should_return_fizzbuzz() {
+        Assert.assertEquals("fizzbuzz", fizzbuzz(15));
+        Assert.assertEquals("fizzbuzz", fizzbuzz(30));
+        Assert.assertEquals("fizzbuzz", fizzbuzz(90));
+    }
+
 
     private String fizzbuzz(int i) {
         if (i == 0) return String.valueOf(i);
